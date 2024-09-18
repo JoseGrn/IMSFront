c01040705fb69135b79bcfd70c8e792c262b6828
Marcela Estrada <marcelaestradarl@gmail.com>
2024-09-18 14:07:47 -0600

    Estilo a la pagina de bienvenida y login

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,37 @@
-// src/pages/Home.js
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import Button from '../components/Button/Button';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button/Button";
+import image1 from "../assets/image1.jpg";
+import image2 from "../assets/image2.jpg";
+import image3 from "../assets/image3.png";
 
 const Home = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const handleLoginClick = () => {
-    navigate('/login');  // Redirige a la página de login
-  };
+	const handleLoginClick = () => {
+		navigate("/login");
+	};
 
-  return (
-    <div>
-      <div className="container">
-        <h1>Bienvenido a la página principal</h1>
-        <Button text="Login" onClick={handleLoginClick} />
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="carousel">
+				<div className="carousel-images">
+					<img src={image1} alt="Image1" />
+					<img src={image2} alt="Image2" />
+					<img src={image3} alt="Image3" />
+				</div>
+				<div className="carousel-images">
+					<img src={image1} alt="Image1" />
+					<img src={image2} alt="Image2" />
+					<img src={image3} alt="Image3" />
+				</div>
+			</div>
+			<div className="home-container">
+				<h1>Bienvenido</h1>
+				<Button text="Iniciar sesión" onClick={handleLoginClick} />
+			</div>
+		</div>
+	);
 };
 
 export default Home;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,132 +1,135 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Para redirigir a otra página
-import { Modal, Button } from 'react-bootstrap';  // Importar los componentes de modal
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Modal, Button } from "react-bootstrap";
+import logo from "../assets/IMS.png";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [isEmpresaMode, setIsEmpresaMode] = useState(true);
-  const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [modalMessage, setModalMessage] = useState('');
-  const [showModal, setShowModal] = useState(false);
-  const navigate = useNavigate(); // Para redirigir al usuario
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [isEmpresaMode, setIsEmpresaMode] = useState(true);
+	const [error, setError] = useState("");
+	const [loading, setLoading] = useState(false);
+	const [modalMessage, setModalMessage] = useState("");
+	const [showModal, setShowModal] = useState(false);
+	const navigate = useNavigate();
 
-  const handleCloseModal = () => setShowModal(false);
+	const handleCloseModal = () => setShowModal(false);
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    setError('');
+	const handleLogin = async (e) => {
+		e.preventDefault();
+		setLoading(true);
+		setError("");
 
-    const endpoint = isEmpresaMode
-      ? 'http://localhost:6001/api/Owner/loginowner'
-      : 'http://localhost:6001/api/User/loginuser';
+		const endpoint = isEmpresaMode
+			? "http://localhost:6001/api/Owner/loginowner"
+			: "http://localhost:6001/api/User/loginuser";
 
-    const params = new URLSearchParams({
-      user: username,
-      password: password,
-    });
+		const params = new URLSearchParams({
+			user: username,
+			password: password,
+		});
 
-    try {
-      const response = await fetch(`${endpoint}?${params}`, {
-        method: 'GET',
-      });
+		try {
+			const response = await fetch(`${endpoint}?${params}`, {
+				method: "GET",
+			});
 
-      if (!response.ok) {
-        const message = await response.text();
-        throw new Error(message || 'Error en la autenticación');
-      }
+			if (!response.ok) {
+				const message = await response.text();
+				throw new Error(message || "Error en la autenticación");
+			}
 
-      const data = await response.json();
-      console.log('Datos recibidos:', data);
+			const data = await response.json();
+			console.log("Datos recibidos:", data);
 
-      if (isEmpresaMode) {
-        // Guardar ownerId, firstName y lastName en localStorage
-        localStorage.setItem('ownerId', data.ownerId);
-        localStorage.setItem('firstName', data.firstName);
-        localStorage.setItem('lastName', data.lastName);
-        
-        // Redirigir a la página de empresas
-        navigate('/empresas');
-      } else {
-        // Guardar información de usuario en localStorage
-        localStorage.setItem('userId', data.userId);
-        localStorage.setItem('companyId', data.companyId);
-        localStorage.setItem('username', data.username);
-        localStorage.setItem('name', data.name);
-        localStorage.setItem('role', data.role);
-        localStorage.setItem('productsIdList', data.productsIdList);
-        
-        // Redirigir a la página de Usuario
-        navigate('/usuario');
-      }
-    } catch (error) {
-      console.error('Error al hacer login:', error);
-      setError('Credenciales incorrectas o error en el servidor.');
-      setModalMessage('Error: Credenciales incorrectas o error en el servidor.');
-      setShowModal(true);  // Mostrar modal de error
-    } finally {
-      setLoading(false);
-    }
-  };
+			if (isEmpresaMode) {
+				localStorage.setItem("ownerId", data.ownerId);
+				localStorage.setItem("firstName", data.firstName);
+				localStorage.setItem("lastName", data.lastName);
+				navigate("/empresas");
+			} else {
+				localStorage.setItem("userId", data.userId);
+				localStorage.setItem("companyId", data.companyId);
+				localStorage.setItem("username", data.username);
+				localStorage.setItem("name", data.name);
+				localStorage.setItem("role", data.role);
+				localStorage.setItem("productsIdList", data.productsIdList);
+				navigate("/usuario");
+			}
+		} catch (error) {
+			console.error("Error al hacer login:", error);
+			setError("Credenciales incorrectas o error en el servidor.");
+			setModalMessage(
+				"Error: Credenciales incorrectas o error en el servidor."
+			);
+			setShowModal(true);
+		} finally {
+			setLoading(false);
+		}
+	};
 
-  const toggleMode = () => {
-    setIsEmpresaMode(!isEmpresaMode);
-  };
+	const toggleMode = () => {
+		setIsEmpresaMode(!isEmpresaMode);
+	};
 
-  return (
-    <div>
-      <div className="container">
-        <h1>Iniciar sesión</h1>
-        <div className="toggle-button" onClick={toggleMode}>
-          <div className={isEmpresaMode ? 'toggle-button active' : 'toggle-button'}></div>
-        </div>
-        <p>{isEmpresaMode ? 'Modo Empresa' : 'Modo Usuario'}</p>
-        <form className="login-form" onSubmit={handleLogin}>
-          <div>
-            <label htmlFor="username">Usuario:</label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder="Ingresa tu usuario"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Contraseña:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Ingresa tu contraseña"
-              required
-            />
-          </div>
-          <button type="submit" disabled={loading}>
-            {loading ? 'Cargando...' : 'Login'}
-          </button>
-          {error && <p style={{ color: 'red' }}>{error}</p>}
-        </form>
-      </div>
+	return (
+		<div>
+			<div className="container">
+				<div className="logo-container">
+					<img src={logo} alt="IMS" className="logo" />
+				</div>
 
-      {/* Modal para mostrar el mensaje */}
-      <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Mensaje</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>{modalMessage}</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>
-            Cerrar
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
-  );
+				<div className="toggle-button" onClick={toggleMode}>
+					<div
+						className={
+							isEmpresaMode ? "toggle-button active" : "toggle-button"
+						}></div>
+				</div>
+				<p>{isEmpresaMode ? "Modo Empresa" : "Modo Usuario"}</p>
+
+				<form className="login-form" onSubmit={handleLogin}>
+					<div>
+						<label htmlFor="username">Usuario:</label>
+						<input
+							type="text"
+							id="username"
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+							placeholder="Ingresa tu usuario"
+							required
+						/>
+					</div>
+					<div>
+						<label htmlFor="password">Contraseña:</label>
+						<input
+							type="password"
+							id="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							placeholder="Ingresa tu contraseña"
+							required
+						/>
+					</div>
+					<button type="submit" disabled={loading}>
+						{loading ? "Cargando..." : "Login"}
+					</button>
+					{error && <p style={{ color: "red" }}>{error}</p>}
+				</form>
+			</div>
+
+			<Modal show={showModal} onHide={handleCloseModal}>
+				<Modal.Header closeButton>
+					<Modal.Title>Mensaje</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>{modalMessage}</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleCloseModal}>
+						Cerrar
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</div>
+	);
 };
 
 export default Login;
